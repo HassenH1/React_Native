a89a38cada76ef54169d7dd99aa5be6a1dc7051b
HassenH1 <hasansaid51@gmail.com>
2020-03-31 16:34:01 -0700

    testing something out

--- components/Header/index.js
+++ components/Header/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { AntDesign, Ionicons, MaterialIcons } from '@expo/vector-icons'
 import Constant from 'expo-constants'
+import { useNavigation } from '@react-navigation/native'
+
 
 const Header = () => {
   return (
     <View style={{
       height: 45,
-      backgroundColor: "fff",
+      backgroundColor: "white",
       flexDirection: "row",
       justifyContent: "space-between",
       elevation: 4, //shadowing only works for andriod
@@ -22,11 +24,28 @@ const Header = () => {
 
       <View style={{
         flexDirection: "row",
+        margin: 5
+      }}>
+        <AntDesign name="youtube" size={32} color="red" style={{
+          marginLeft: 20
+        }} />
+        <Text style={{
+          fontSize: 22,
+          marginLeft: 5,
+          fontWeight: "bold",
+          color: "red"
+        }}>Youtube</Text>
+      </View>
+
+      <View style={{
+        flexDirection: "row",
         justifyContent: "space-around",
         width: 150,
         margin: 5
       }}>
-        <Ionicons name="md-search" size={32} color="black" />
+        <Ionicons name="md-videocam" size={32} color="red" />
+        <Ionicons name="md-search" size={32} color="red" onPress={() => navigation.navigate("search")} />
+        <MaterialIcons name="account-circle" size={32} color="black" />
       </View>
     </View>
   )
