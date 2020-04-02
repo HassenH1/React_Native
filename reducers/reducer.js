65849ec132089df399adc6d92989908377a68165
HassenH1 <hasansaid51@gmail.com>
2020-04-01 17:27:13 -0700

    express validator

--- reducers/reducer.js
+++ reducers/reducer.js
@@ -10,24 +10,40 @@ export const reducer = (state = initialState, action) => {
 
   switch (action.type) {
     case "ADDING":
-      const addUser = fetch(`${url}/signup`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'Application/json'
-        },
-        body: JSON.stringify(action.payload)
-      })
-      console.log(action.payload, "<-------------what is this payload?")
+
+        fetch(`${url}/signup`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'Application/json'
+          },
+          body: JSON.stringify(action.payload)
+        })
+        .then((res) => res.json())
+        .then((result) => {
+          // console.log(result, "<--------result")
+          if(result.errors){
+            result.errors.map((elem) => {
+              console.log(elem, "<========does this work")
+              console.log(elem.msg)
+              console.log(elem.param)
+            })
+          }
+        })
+        .catch((err) => {
+          console.log(err, "<----------------the catch method?")
+        })
+
       return {
         ...state,
         user: action.payload
       }
+
     case "LOADING":
       return {
         ...state,
         loading: action.payload
       }
-    case "GET_USER": 
+    case "GET_USER":
       console.log(action.payload, "<-------------getting user")
       return {
         ...state,
@@ -41,3 +57,7 @@ export const reducer = (state = initialState, action) => {
   }
   return state
 }
+
+// errors: Array(2)
+// 0: {value: "h", msg: "Invalid value", param: "username", location: "body"}
+// 1: {value: "h", msg: "Invalid value", param: "password", location: "body"
--- server/controller/user.js
+++ server/controller/user.js
@@ -1,10 +1,24 @@
 const express = require("express")
 const router = express.Router()
+const { check, validationResult } = require('express-validator');
+
 
 const User = require("../model/User.js")
 
-router.post("/signup", async (req, res) => {
-  console.log(req.body, "<--------body")
+router.post("/signup", [
+  // username must be an email
+  check('email').isEmail(),
+  check('username').isLength({ min: 3 }),
+  // password must be at least 5 chars long
+  check('password').isLength({ min: 5 })
+], (req, res) => {
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  console.log("the end of the post method?")
+
   // try {
   //   const createUser = await User.create(req.body)
 
@@ -24,13 +38,13 @@ router.post("/signin", async (req, res) => {
     if (get === null) {
 
       res.status(400).send({
-      
+
         message: 'This is an error!'
-      
+
       });
-      
+
       return
-    
+
     } else {
 
       res.json(get)
